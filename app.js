a3e1a4e8f769512577425ec3286b89d493e3740d
oscar <oscaroceguera@gmail.com>
2016-10-21 23:36:13 -0600

    finish

---
+++ app.js
@@ -0,0 +1,51 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
+const config = require('config')
+const handleErrors = require('./middlewares/handleErrors')
+const db = require('./db')
+
+const app = new Koa()
+const router = new Router()
+
+app.use(handleErrors)
+app.use(bodyParser())
+
+router.get('/error/test', async () => {
+  throw Error('Error handling works!')
+})
+
+router.get('/', (ctx) => ctx.body = {hello: 'world'})
+
+router.post('/birds', async (ctx, next) => {
+  const data = ctx.request.body
+  ctx.body = await db.Bird.insertOne(data)
+})
+
+router.get('/birds/:id', async (ctx, next) => {
+  const id = ctx.params.id;
+  ctx.body = await db.Bird.findOneById(id)
+})
+
+router.put('/birds/:id', async (ctx, next) => {
+  const id = ctx.params.id;
+  const data = ctx.request.body
+  ctx.body = await db.Bird.findOneAndUpdate(id, data)
+})
+
+router.delete('/birds/:id', async (ctx, next) => {
+  const id = ctx.params.id;
+  ctx.body = await db.Bird.removeOne(id)
+})
+
+app.use(router.routes())
+
+db.connect()
+  .then(() => {
+    app.listen(config.port, () => {
+      console.info(`Listen to http://localhost:${config.port}`)
+    })
+  })
+  .catch((err) => {
+    console.log('ERR:', err);
+  })
---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const MongoClient = require('mongodb').MongoClient;
+const config = require('config');
+const Model = require('./model');
+let db
+
+class Db {
+	async connect() {
+		if (!db) {
+			db = await MongoClient.connect(config.db.url);
+			this.Bird = new Model(db, 'birds');
+		}
+	}
+};
+
+module.exports = new Db();
